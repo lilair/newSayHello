531300f0a03acedf90d7b3cf77f4571ab0b0ca1e
lilair <1130270216@qq.com>
2019-10-09 15:21:35 +0800

    第二次commit

--- sayHello/src/com/changhong/controller/sayHello.java
+++ sayHello/src/com/changhong/controller/sayHello.java
@@ -3,5 +3,8 @@ package com.changhong.controller;
 public class sayHello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
     }
 }
